e569561d75c0b15e44ac06c1bc6de6aa3323752f
Gopi3998 <91726200+Gopi3998@users.noreply.github.com>
2021-10-18 07:52:51 -0700

    Add files via upload

---
+++ REVERSE A STRING.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print('Starting of the program for reverse a string'.center(80,'*'))
+string = '1234abcd'
+rev_str = string[::-1]
+print('The given string is',string)
+print('The reversed string is',rev_str)
+print('THE END'.center(80,'*'))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
